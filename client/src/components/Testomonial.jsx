fef37eb07ab2fe41a965684474530c9ea92fcb04
Haris <mh47098@gmail.com>
2025-05-01 13:59:56 +0100

    add Testemonial and NewsLetter

---
+++ client/src/components/Testomonial.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Title from "./Title";
+import { testimonials, assets } from "../assets/assets";
+import StarRating from "./StarRating";
+const Testomonial = () => {
+  return (
+    <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 pt-20 pb-30">
+      <Title
+        title="What Our Guests Say"
+        subtitle="Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories."
+      />
+      <div className="flex flex-wrap items-center  gap-6 mt-20 ">
+        {testimonials.map((testimonial) => (
+          <div key={testimonial.id} className="bg-white p-6 rounded-xl shadow ">
+            <div className="flex items-center gap-3">
+              <img
+                className="w-12 h-12 rounded-full"
+                src={testimonial.image}
+                alt={testimonial.name}
+              />
+              <div>
+                <p className="font-playfair text-xl">{testimonial.name}</p>
+                <p className="text-gray-500">{testimonial.address}</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-1 mt-4">
+              <StarRating />
+            </div>
+            <p className="text-gray-500 max-w-90 mt-4">
+              "{testimonial.review}"
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Testomonial;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Hero from "../components/Hero";
 import FeatureDestinations from "../components/FeatureDestinations";
 import Title from "../components/Title";
 import ExclusiveOffers from "../components/ExclusiveOffers";
+import Testomonial from "../components/Testomonial";
+import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Title />
       <FeatureDestinations />
       <ExclusiveOffers />
+      <Testomonial />
+      <NewsLetter />
     </div>
   );
 };
